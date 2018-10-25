f2c6914083dd4082ae8b768516a41e188ac9f56a
Hasol Im <jerubball@gmail.com>
2018-10-25 18:30:19 -0400

    Create nyit.py

---
+++ mirror/scripts/nyit.py
@@ -0,0 +1,33 @@
+
+
+class Nyit(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.config(bg='black')
+        self.title = 'News' # 'News' is more internationally generic
+        self.newsLbl = Label(self, text=self.title, font=('Helvetica', medium_text_size), fg="white", bg="black")
+        self.newsLbl.pack(side=TOP, anchor=W)
+        self.headlinesContainer = Frame(self, bg="black")
+        self.headlinesContainer.pack(side=TOP)
+        self.get_headlines()
+
+    def get_headlines(self):
+        try:
+            # remove all children
+            for widget in self.headlinesContainer.winfo_children():
+                widget.destroy()
+            if news_country_code == None:
+                headlines_url = "https://news.google.com/news?ned=us&output=rss"
+            else:
+                headlines_url = "https://news.google.com/news?ned=%s&output=rss" % news_country_code
+
+            feed = feedparser.parse(headlines_url)
+
+            for post in feed.entries[0:5]:
+                headline = NewsHeadline(self.headlinesContainer, post.title)
+                headline.pack(side=TOP, anchor=W)
+        except Exception as e:
+            traceback.print_exc()
+            print "Error: %s. Cannot get news." % e
+
+        self.after(600000, self.get_headlines)
