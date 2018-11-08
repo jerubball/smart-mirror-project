ab163a230e7d7a643468d8cb3d4af0dd7a1b185e
Hasol Im <jerubball@gmail.com>
2018-11-08 18:58:53 -0500

    Update opencv/Facerec.py

--- opencv/Facerec.py
+++ opencv/Facerec.py
@@ -9,11 +9,12 @@ import numpy as np
 import time
 
 home_dir = 'C:\\Users\\Timmy .LAPTOP-VEVB5SPR\Desktop\\'
+
 #function to detect face using OpenCV
 def detect_face(img):
     #convert the test image to gray scale as opencv face detector expects gray images
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    
     #load OpenCV face detector, I am using LBP which is fast
     #there is also a more accurate but slow: Haar classifier
     #face_cascade = cv2.CascadeClassifier('faces/lbpcascade_frontalface_improved.xml')
@@ -22,74 +23,71 @@ def detect_face(img):
     #let's detect multiscale images(some images may be closer to camera than others)
     #result is a list of faces
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
-
+    
     #if no faces are detected then return original img
     if (len(faces) == 0):
         print("face not found")
         return None, None
-
+    
     #under the assumption that there will be only one face,
     #extract the face area
     (x, y, w, h) = faces[0]
     return gray[y:y+w, x:x+h], faces[0]
 
 def prepare_training_data(data_folder_path):
-
-
     # ------STEP-1--------
     # get the directories (one directory for each subject) in data folder
     dirs = os.listdir(data_folder_path)
-
+    
     # list to hold all subject faces
     faces = []
     # list to hold labels for all subjects
     labels = []
-
+    
     # let's go through each directory and read images within it
     for dir_name in dirs:
         print (dir_name)
-
+        
         # our subject directories start with letter 's' so
         # ignore any non-relevant directories if any
         if not dir_name.startswith("s"):
             continue;
-
+        
         # ------STEP-2--------
         # extract label number of subject from dir_name
         # format of dir name = slabel
         # , so removing letter 's' from dir_name will give us label
         label = int(dir_name.replace("s", ""))
-
+        
         # build path of directory containing images for current subject subject
         # sample subject_dir_path = "training-data/s1"
         subject_dir_path = data_folder_path + "/" + dir_name
-
+        
         # get the images names that are inside the given subject directory
         subject_images_names = os.listdir(subject_dir_path)
-
+        
         # ------STEP-3--------
         # go through each image name, read image,
         # detect face and add face to list of faces
         for image_name in subject_images_names:
-
             # ignore system files like .DS_Store
             if image_name.startswith("."):
                 continue;
-
-        # build image path
-        # sample image path = training-data/s1/1.pgm
+            
+            # build image path
+            # sample image path = training-data/s1/1.pgm
             image_path = subject_dir_path + "/" + image_name
-
-        # read image
+            
+            # read image
             image = cv2.imread(image_path)
-
-        # display an image window to show the image
+            
+            # display an image window to show the image
             cv2.imshow("Training on image...", image)
             cv2.waitKey(100)
-        # detect face
-        # detect face
+            # detect face
+            # detect face
             face, rect = detect_face(image)
-
+        
         # ------STEP-4--------
         # for the purpose of this tutorial
         # we will ignore faces that are not detected
@@ -98,13 +96,12 @@ def prepare_training_data(data_folder_path):
                 faces.append(face)
                 # add label for this face
                 labels.append(label)
-
+            
             cv2.destroyAllWindows()
             cv2.waitKey(1)
             cv2.destroyAllWindows()
-
+    
     return faces, labels
-
 
 # function to draw rectangle on image
 # according to given (x, y) coordinates and
@@ -119,33 +116,28 @@ def draw_rectangle(img, rect):
 def draw_text(img, text, x, y):
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
-
 # this function recognizes the person in image passed
 # and draws a rectangle around detected face with name of the
 # subject
 def predict(test_img):
-
-
     # make a copy of the image as we don't want to change original image
     img = test_img.copy()
     # detect face from the image
     face, rect = detect_face(img)
     #print(face)
-
+    
     # predict the image using our face recognizer
     label = face_recognizer.predict(face)
     print(label)
     # get name of respective label returned by face recognizer
     label_text = labels[label[0]] #subjects[label]
-
+    
     # draw a rectangle around face detected
     draw_rectangle(img, rect)
     # draw name of predicted person
     # draw_text(img, label_text, rect[0], rect[1] - 5)
-
+    
     return img
-
-
 
 # let's first prepare our training data
 # data will be in two lists of same size
@@ -166,9 +158,9 @@ face_recognizer = cv2.createLBPHFaceRecognizer()
 
 # or use EigenFaceRecognizer by replacing above line with
 #face_recognizer = cv2.face.createEigenFaceRecognizer()
-
 # or use FisherFaceRecognizer by replacing above line with
 #face_recognizer = cv2.face.createFisherFaceRecognize()
+
 #face_recognizer = cv2.face.createFisherFaceRecognize()
 #face_recognizer = cv2.face_EigenFaceRecognizer.create()
 #face_recognizer = cv2.face_LBPHFaceRecognizer.create()
@@ -191,7 +183,7 @@ for pair in test_files:
     test_img_temp = cv2.imread(pair['path'])
     pair['img'] = test_img_temp
     #perform a prediction
-    pair['predict'] = predict(test_img)
+    pair['predict'] = predict(test_img_temp)
 
 print("Prediction complete")
 
